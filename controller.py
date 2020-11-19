0873fa780dc389774e644e4e8973b05ba069912c
MariiaRomanuik <romanuik7meri.ua@gmail.com>
2020-11-19 19:48:20 +0200

    fix none type in abort command

--- controller.py
+++ controller.py
@@ -8,15 +8,12 @@ from database import get_user_by_chat_id, delete_current_status, get_current_sta
 
 @bot.message_handler(Text(equals=["Відмінити"]))
 async def cmd_start(message: Message):
-    try:
-        user = get_user_by_chat_id(message.chat.id)
-        status = get_current_status(message.chat.id)
-        if status.status_type in [StatusType.CreatePersonName.value,
-                                  StatusType.CreatePersonBirthday.value,
-                                  StatusType.CreatePersonCategory.value]:
-            delete_person_by_id(status.person_id)
-        delete_current_status(message.chat.id)
+    user = get_user_by_chat_id(message.chat.id)
+    status = get_current_status(message.chat.id)
+    if status is not None and status.status_type in [StatusType.CreatePersonName.value,
+                                                     StatusType.CreatePersonBirthday.value,
+                                                     StatusType.CreatePersonCategory.value]:
+        delete_person_by_id(status.person_id)
+    delete_current_status(message.chat.id)
 
-        await message.answer(f"{user.name}, вибери варіант нижче:", reply_markup=keyboards.get_menu_keyboard())
-    except():
-        None
+    await message.answer(f"{user.name}, вибери варіант нижче:", reply_markup=keyboards.get_menu_keyboard())
