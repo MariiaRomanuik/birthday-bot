fdd210477d11affdbedd7d0763a66081bf63b614
MariiaRomanuik <romanuik7meri.ua@gmail.com>
2020-12-17 19:55:58 +0200

    comment 2 lines

--- person_controller.py
+++ person_controller.py
@@ -140,8 +140,9 @@ async def delete_person(message: Message):
 @bot.message_handler(lambda message: get_current_status_type(message.chat.id) == StatusType.DeletePerson.value)
 async def delete_person_by_status(message):
     user = get_user_by_chat_id(message.chat.id)
-    status = get_current_status(message.chat.id)
-    delete_person_by_name(status.person.name)
+    # status = get_current_status(message.chat.id)
+    # status.person.name = message.answer
+    delete_person_by_name(message.answer)
     delete_current_status(message.chat.id)
     await message.answer(f"Особа видалена!\n{user.name}, вибери варіант нижче:",
                          reply_markup=keyboards.get_menu_keyboard())
