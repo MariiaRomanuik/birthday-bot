b168f0820621f0ff7cde2ba801edd8ed478a4efa
MariiaRomanuik <romanuik7meri.ua@gmail.com>
2020-12-17 16:41:30 +0200

    add save(status)

--- birthday_controller.py
+++ birthday_controller.py
@@ -123,11 +123,12 @@ async def choose_data(message: Message):
 
 @bot.message_handler(lambda message: get_current_status_type(message.chat.id) == StatusType.ShowBirthdays.value)
 async def shows_birthdays(message: Message):
-    persons = get_persons_by_user(message.chat.id)
     status = get_current_status_type(message.chat.id)
+    status.status_type = StatusType.ShowBirthdaysByDate.value
+    persons = get_persons_by_user(message.chat.id)
     result_message = ""
     birthday = message.text.split(".")
-    status.status_type = StatusType.ShowBirthdaysByDate.value
+    save(status)
 
     try:
         input_day = int(birthday[0])
